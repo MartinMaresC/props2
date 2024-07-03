2c3c61ce8ca192f252e19a22361f755d0bb37276
Martin Mares <78748150+MartinMaresC@users.noreply.github.com>
2024-07-02 19:45:14 -0600

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import imagen from './imagenpokemon.webp';
 import './App.css';
+import React from 'react';
+
+function Pokemon(props) {
+  return (
+    <div style={
+      {
+        backgroundColor: props.color,
+      }
+    }>
+    <img src={props.pokemonImagen}></img>
+    <p>Pokemon: {props.name}</p>
+  </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pokemon pokemonImagen={imagen} name="Pikachu" color="red"/>
+      <Pokemon pokemonImagen={imagen} name="Pikachu" color="blue"/>
+      <Pokemon pokemonImagen={imagen} name="Pikachu" color="green"/>
     </div>
   );
 }
